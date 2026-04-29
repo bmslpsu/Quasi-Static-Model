93a08ea156a50d3bf12cb14b882c03e040508b0b
Eric Jurado <exj5139@psu.edu>
2026-04-29 14:59:06 -0400

    temporarily fix main.m not finding Utils on path

--- main.m
+++ main.m
@@ -8,6 +8,8 @@ clc              % Clear command window
 warning off      % Suppress all warnings
 close all hidden % Close all figures, including minimized ones
 
+addpath(genpath("Utils/")) % temporary fix to find Utils functions
+
 %% Step 2: Runtime Timestamp
 current_time = datetime;
 
